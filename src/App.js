17f539eefeb1cfc30ce3e1a8002722794531b6e6
Michael Campbell <michaelmichael323@hotmail.com>
2022-09-13 16:25:34 -0600

    finished

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import './App.css'
+import Newsletter from './Newsletter.js'
+import { ThemeProvider } from './ThemeContext.js'
+
+const theme = {
+  header: {
+    fg: 'white',
+  },
+  input: {
+    color: '#fff',
+    background: '#070222',
+    textAlign: 'center',
+  },
+  inputFocus: {
+    outline: '2px solid #5e9fff',
+  }
+}
+
+function App() {
+  return (
+    <div className="app__newsletter">
+      <ThemeProvider theme={theme}>
+        <Newsletter />
+      </ThemeProvider>
+    </div>
+  )
+}
+
+export default App
